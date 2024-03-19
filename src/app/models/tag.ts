a192323871aea188a30bd3755bd8f25e4e993544
bazman11 <bazman1@etf.unsa.ba>
2024-03-19 02:33:48 +0100

    do resume

--- src/app/models/tag.ts
+++ src/app/models/tag.ts
@@ -9,6 +9,9 @@ export class Tag{
   static readonly CSHARP = new Tag('C#', 'green');
   static readonly TYPESCRIPT = new Tag('Typescript', 'brown');
   static readonly NODEJS = new Tag('Nodejs', 'darkolivegreen');
+  static readonly JAVA = new Tag('Java', 'magenta');
+  static readonly ASPNET = new Tag('Java', 'black');
+  static readonly JAVASCRIPT = new Tag('Java', 'cyan');
 
   toString(){
     return this.key;
--- src/app/nav/portfolio/portfolio.component.ts
+++ src/app/nav/portfolio/portfolio.component.ts
@@ -8,17 +8,83 @@ import { ProjectsService } from 'src/app/services/projects.service';
 @Component({
   selector: 'app-portfolio',
   templateUrl: './portfolio.component.html',
-  styleUrls: ['./portfolio.component.css']
+  styleUrls: ['./portfolio.component.css'],
 })
 export class PortfolioComponent implements OnInit {
-  constructor(private titleService: Title, private projectService: ProjectsService) {
+  projects = {} as Project[];
+  isCollapsed: boolean = true;
+  typescript: boolean = false;
+  javascript: boolean = false;
+  python: boolean = false;
+  csharp: boolean = false;
+  java: boolean = false;
+  angular: boolean = false;
+  nodejs: boolean = false;
+  aspnet: boolean = false;
+  react: boolean = false;
+  filtering: boolean = false;
 
-   }
+  constructor(
+    private titleService: Title,
+    private projectService: ProjectsService
+  ) {}
 
   ngOnInit() {
     this.titleService.setTitle('Benjamin Ažman - Projects');
     this.projects = this.projectService.getProjects();
   }
 
-  projects = {} as Project[];
+  filter(){
+    let filterTags: Tag[] = [];
+
+    if(this.typescript){
+      filterTags.push(Tag.TYPESCRIPT);
+    }
+    if(this.angular){
+      filterTags.push(Tag.ANGULAR);
+    }
+    if(this.python){
+      filterTags.push(Tag.PYTHON);
+    }
+    if(this.csharp){
+      filterTags.push(Tag.CSHARP);
+    }
+    if(this.java){
+      filterTags.push(Tag.JAVA);
+    }
+    if(this.nodejs){
+      filterTags.push(Tag.NODEJS);
+    }
+    if(this.aspnet){
+      filterTags.push(Tag.ASPNET);
+    }
+    if(this.javascript){
+      filterTags.push(Tag.JAVASCRIPT);
+    }
+    if(this.react){
+      filterTags.push(Tag.REACT);
+    }
+
+    if(this.python || this.csharp || this.java || this.javascript || this.angular || this.typescript || this.nodejs || this.aspnet || this.react){
+      this.filtering=true;
+    }
+
+    this.projects = this.projectService.getProjectsByFilter(filterTags);
+  }
+
+  resetFilters(){
+    this.angular=false;
+    this.aspnet=false;
+    this.python=false;
+    this.csharp=false;
+    this.typescript=false;
+    this.nodejs=false;
+    this.javascript=false;
+    this.java=false;
+    this.react=false;
+    this.filtering=false;
+
+    this.projects = this.projectService.getProjects();
+  }
+
 }
